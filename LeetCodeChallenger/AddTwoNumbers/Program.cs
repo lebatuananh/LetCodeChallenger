396fca4f6f70c939d705d5a0cefd486710a0e8c0
Le Ba Tuan Anh <tuananh300496@gmail.com>
2021-08-19 09:44:43 +0700

    Two Sum

---
+++ LeetCodeChallenger/AddTwoNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+/********************************************************************
+ * You are given two non-empty linked lists representing two non-negative integers.
+ * The digits are stored in reverse order, and each of their nodes contains a single digit.
+ * Add the two numbers and return the sum as a linked list.
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ * Input: l1 = [2,4,3], l2 = [5,6,4]
+ * Output: [7,0,8]
+ * Explanation: 342 + 465 = 807.
+ ********************************************************************/
+
+
+namespace AddTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
--- LeetCodeChallenger/TwoSum/Program.cs
+++ LeetCodeChallenger/TwoSum/Program.cs
@@ -1,4 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+
+/************************************************************************************
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+ *
+ * Example 1:
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+ ************************************************************************************/
 
 namespace TwoSum
 {
@@ -6,7 +18,51 @@ namespace TwoSum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Print(new[] { 2, 7, 11, 15 }, 18);
+            Console.ReadKey();
+        }
+
+        private static void Print(int[] nums, int target)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            var results = Solution.TwoSum(nums, target);
+            watch.Stop();
+            if (results.Length == 2)
+            {
+                Console.WriteLine($"1. Two Sums\n" +
+                                  $"Input Array = {PrintInputArray(nums)} target = {target}\n" +
+                                  $"Result: [{results[0]}, {results[1]}] \n" +
+                                  $"Execution Speed: {watch.ElapsedMilliseconds}ms \n");
+            }
+        }
+
+        private static string PrintInputArray<T>(T[] input)
+        {
+            string result = "[ ";
+            for (int i = 0; i <= input.Length - 1; i++)
+            {
+                result += $"{input[i]} ";
+            }
+
+            result += "]";
+            return result;
+        }
+    }
+
+    public static class Solution
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            var dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int diff = target - nums[i];
+                if (dict.ContainsKey(diff))
+                    return new[] { i, dict[diff] };
+                dict.TryAdd(nums[i], i);
+            }
+
+            return new[] { 0, 0 };
         }
     }
 }
